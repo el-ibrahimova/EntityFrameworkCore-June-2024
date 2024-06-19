ac80e2fa1ed23381b0394901c2d012b738c6bd38
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-19 14:15:35 +0300

    Initial commit

---
+++ 01.ORM-Fundamentals-Lab/Demo/ADO.Net-Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+/* first we need CONNECTION*/
+string connectionString = "Server=.;Database=LabSoftUni; User=DESKTOP-SENJ7PO;Password=...;TrustServerCertificate=true";
+
+/* then we need QUERY to execute*/
+string query = "SELECT EmployeeID, FirstName, LastName, JobTitle, FROM Employees WHERE DepartmentID = @departmentId";
+
+/* add parameter */
+int departmentId = 3;
+
+/* create connection*/
+using SqlConnection connection = new SqlConnection(connectionString);
+
+SqlCommand cmd = new SqlCommand(query, connection);
+cmd.Parameters.AddWithValue("@departmentId", departmentId);
+
+try
+{
+    /*  open connection */
+    connection.Open();
+
+    /* last we create READER */
+    SqlDataReader reader = cmd.ExecuteReader();
+
+    while (reader.Read())
+    {
+        Console.WriteLine($"{reader[1]} {reader[2]}: {reader[3]}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+
+}
