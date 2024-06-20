b66d94f66714fa2bbfb1c52d088d1975555efb5f
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-20 11:20:37 +0300

    create MiniORM.App

---
+++ 02. ORM-Fundamentals-Exercise/MiniORM.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using MiniORM;
+using MiniORM.App.Entities;
+
+var departments = new Department[]
+{
+    new Department { Id = 1, Name = "First" },
+    new Department { Id = 2, Name = "Second" }
+};
+
+var changeTracker = new ChangeTracker<Department>(departments);
+
+Console.WriteLine();
