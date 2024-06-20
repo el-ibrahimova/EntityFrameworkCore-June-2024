a46e3ddf13b610d4b5a382b62980352c12a85450
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-20 11:19:46 +0300

    add MiniORM project

---
+++ 02. ORM-Fundamentals-Exercise/MiniORM/ChangeTracker.cs
@@ -0,0 +1,46 @@
+﻿namespace MiniORM
+{
+    public class ChangeTracker<T> 
+    where T : class, new()
+    {
+        private readonly List<T> _allEntities;
+        private readonly List<T> _added;
+        private readonly List<T> _removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            if(entities is  null)
+                throw new ArgumentNullException(nameof(entities));
+
+            this._added = new List<T>();
+            this._removed = new List<T>();
+            this._allEntities = CloneEntities(entities).ToList();
+        }
+
+        public IReadOnlyCollection<T> Added => this._added.AsReadOnly();
+        public IReadOnlyCollection<T> Removed => this._removed.AsReadOnly();
+
+        private static IEnumerable<T> CloneEntities(IEnumerable<T> entities)
+        {
+            var properties = typeof(T).GetAllowedSqlProperties();
+
+            List<T> result = new List<T>();
+
+            foreach (var originaEntity in entities)
+            {
+               // var copy = Activator.CreateInstance<T>();
+               var copy = new T();
+
+               foreach (var property in properties)
+               {
+                   var value = property.GetValue(originaEntity);
+                   property.SetValue(copy, value);
+               }
+
+               result.Add(copy);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 02. ORM-Fundamentals-Exercise/MiniORM/DbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniORM
+{
+    public class DbContext
+    {
+        internal static HashSet<Type> AllowedSqlTypes { get; } = new HashSet<Type>
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime)
+        };
+    }
+}
