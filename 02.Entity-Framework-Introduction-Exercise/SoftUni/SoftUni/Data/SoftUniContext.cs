a2c0c384849a56e16237f943d26e110ec01b3b17
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-21 19:48:40 +0300

    update

--- 02.Entity-Framework-Introduction-Exercise/SoftUni/SoftUni/Data/SoftUniContext.cs
+++ 02.Entity-Framework-Introduction-Exercise/SoftUni/SoftUni/Data/SoftUniContext.cs
@@ -20,6 +20,7 @@ namespace SoftUni.Data
         public virtual DbSet<Employee> Employees { get; set; } = null!;
         public virtual DbSet<Project> Projects { get; set; } = null!;
         public virtual DbSet<Town> Towns { get; set; } = null!;
+        public virtual DbSet<EmployeeProject> EmployeesProjects { get; set; } = null;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
