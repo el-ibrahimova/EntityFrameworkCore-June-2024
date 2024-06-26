7c2b6298dfe87de3173ae594b804fa4580eab012
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-26 15:44:58 +0300

    create Data with StudentSystemContext

---
+++ 03.Entity-Relations-Exercise/P01_StudentSystem/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext:DbContext
+    {
+        private const string ConnectionString =
+            "Server=DESKTOP-SENJ7PO\\SQLEXPRESS;Database=StudentSystem;Integrated Security=True";
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // за това трябва да имаме инсталиран провайдър Microsoft.EntityFrameworkCore.SqlServer
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+
+
+    }
+}
