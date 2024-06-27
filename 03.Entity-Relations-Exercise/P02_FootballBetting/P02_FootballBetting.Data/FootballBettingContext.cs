39a1b6dcba22704417141931dac5f706e091a3ac
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-27 09:34:19 +0300

    create project

---
+++ 03.Entity-Relations-Exercise/P02_FootballBetting/P02_FootballBetting.Data/FootballBettingContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P02_FootballBetting.Data
+{
+    public class FootballBettingContext:DbContext
+    {
+        // Use it when developing the application
+        // when we test the application locally on our PC
+        public FootballBettingContext()
+        {
+            
+        }
+        
+        // used by Judge
+        // loading of the DbContext with Dependency Injection -> in real application it is useful
+        public FootballBettingContext(DbContextOptions options)
+        : base(options) 
+        {
+            
+        }
+
+        // Connection configuration
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Set default connection string
+                optionsBuilder.UseSqlServer(v)
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        // Fluent API and Entities configuration
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
