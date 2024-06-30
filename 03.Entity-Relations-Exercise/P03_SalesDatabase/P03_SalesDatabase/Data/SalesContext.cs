7786cb04685746be267759480cbee848f307fff4
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-30 10:05:58 +0300

    create class SalesContext

---
+++ 03.Entity-Relations-Exercise/P03_SalesDatabase/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext:DbContext
+    {
+        private const string ConnectionString =
+            "Server=DESKTOP-SENJ7PO\\SQLEXPRESS;Database=SalesDatabase;Integrated Security=True";
+
+        public SalesContext()
+        {
+            
+        }
+
+        public SalesContext(DbContextOptions options)
+        :base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }
+        }
+
+        public DbSet<Product> Products { get; set; }
+         public DbSet<Customer> Customers { get; set; }
+         public DbSet<Store> Stores { get; set; }
+         public DbSet<Sale> Sales { get; set; }
+    }
+}
