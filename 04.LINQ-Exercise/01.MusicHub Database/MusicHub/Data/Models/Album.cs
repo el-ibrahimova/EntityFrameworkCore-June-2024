8cd0c92b8ce9dfc63c59adf820c1f23fadaff6ed
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-30 17:29:40 +0300

    create Models

---
+++ 04.LINQ-Exercise/01.MusicHub Database/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [MaxLength(ValidationConstants.AlbumNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public DateTime ReleaseDate { get; set; }
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId { get; set; }
+        public virtual Producer? Producer { get; set; }
+
+        [NotMapped]
+        // this property doesn't exist in DB
+        public decimal Price => this.Songs.Sum(s => s.Price);
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ 04.LINQ-Exercise/01.MusicHub Database/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MusicHub.Data.Models.Enums;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        public Song()
+        {
+            this.SongPerformers = new HashSet<SongPerformer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(ValidationConstants.SongNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        // TimeSpan ts = new TimeSpan(1,5,55)  => 01:05:55 = part of time
+        // in the DB this will be stored as BIGINT <=> Ticks count
+        // by default is required
+        public TimeSpan Duration { get; set; }
+
+        
+        // by default is required
+        public DateTime CreatedOn { get; set; }
+
+        // enums are stored in the DB as INT
+        // required by default
+        public Genre Genre { get; set; }
+
+
+         [ForeignKey(nameof(Album))]
+        public int? AlbumId { get; set; }
+
+        public virtual Album? Album { get; set; }
+        
+        [ForeignKey(nameof(Writer))]
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+    }
+}
