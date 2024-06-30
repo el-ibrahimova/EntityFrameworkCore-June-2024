3753a45b9a9c7cd3d2bd9461d06c9783bfc1830a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-06-30 17:29:33 +0300

    add class MusicHubDbContext and ValidationConstants

---
+++ 04.LINQ-Exercise/01.MusicHub Database/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,54 @@
+﻿using MusicHub.Data.Models;
+
+namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Song>(entity =>
+            {
+                entity.Property(s => s.CreatedOn)
+                    .HasColumnType("date");
+            });
+
+            builder.Entity<Album>(entity =>
+            {
+                entity.Property(s => s.ReleaseDate)
+                    .HasColumnType("date");
+            });
+
+            builder.Entity<SongPerformer>(entity =>
+            {
+                entity.HasKey(pk => new { pk.PerformerId, pk.SongId });
+            });
+        }
+    }
+}
