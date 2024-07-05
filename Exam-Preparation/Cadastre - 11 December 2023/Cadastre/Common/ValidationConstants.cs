14c963b72e63145c751586adf7423b84f4bb8637
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-05 08:09:38 +0300

    create files

---
+++ Exam-Preparation/Cadastre - 11 December 2023/Cadastre/Common/ValidationConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace Cadastre.Common
+{
+    public static class ValidationConstants
+    {
+        //District
+        public const int DistrictNameMinLength = 2;
+        public const int DistrictNameMaxLength = 80;
+        public const int DistrictPostalCodeMaxLength = 8;
+
+       //Property
+        public const int PropertyIdentifierMinLength = 16;
+        public const int PropertyIdentifierMaxLength = 20;
+        public const int PropertyDetailsMinLength = 5;
+        public const int PropertyDetailsMaxLength = 500;
+        public const int PropertyAddressMinLength = 5;
+        public const int PropertyAddressMaxLength = 200;
+
+        //Citizen
+        public const int CitizenNameMinLength = 2;
+        public const int CitizenNameMaxLength = 30;
+    }
+}
