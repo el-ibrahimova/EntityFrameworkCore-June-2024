96bf93d5e2dbad92ff042aa1e4d59c456662e6fe
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-05 08:08:34 +0300

    create Data

---
+++ Exam-Preparation/Cadastre - 11 December 2023/Cadastre/Data/CadastreContext.cs
@@ -0,0 +1,45 @@
+﻿using Cadastre.Data.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Cadastre.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    public class CadastreContext : DbContext
+    {
+        public CadastreContext()
+        {
+            
+        }
+
+        public CadastreContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        public DbSet<Citizen> Citizens { get; set; } = null!;
+        public DbSet<District> Districts { get; set; } = null!;
+        public DbSet<Property> Properties { get; set; } = null!;
+        public DbSet<PropertyCitizen> PropertiesCitizens { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<District>()
+                .Property(p => p.PostalCode)
+                .IsFixedLength(true)
+                .HasAnnotation("RegularExpression", "^[A-Z]{2}-[0-9]{5}");
+
+            modelBuilder.Entity<PropertyCitizen>()
+                .HasKey(pc => new { pc.CitizenId, pc.PropertyId });
+
+        }
+    }
+}
