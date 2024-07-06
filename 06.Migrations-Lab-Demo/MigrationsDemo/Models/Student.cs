07d52339bc999974dc296a6424f43e9322b9748d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-06 08:56:45 +0300

    create Model Student

---
+++ 06.Migrations-Lab-Demo/MigrationsDemo/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MigrationsDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Age { get; set; }
+
+        [MaxLength(50)]
+        public string Email { get; set; } = null!;
+    }
+}
