810e7e40c1c8359fb66d10478f8e739c5d280684
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-06 08:57:04 +0300

    create Migrations

---
+++ 06.Migrations-Lab-Demo/MigrationsDemo/Migrations/20240705140611_EmailAdded.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrationsDemo.Migrations
+{
+    public partial class EmailAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+        }
+    }
+}
---
+++ 06.Migrations-Lab-Demo/MigrationsDemo/Migrations/20240705152410_StudentFullNameRenamed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrationsDemo.Migrations
+{
+    public partial class StudentFullNameRenamed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Students",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Students",
+                newName: "FullName");
+        }
+    }
+}
