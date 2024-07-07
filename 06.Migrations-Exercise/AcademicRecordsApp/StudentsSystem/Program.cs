3a570f29d7675dfc11fb551d7bf1490252a1eb1d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-07 11:47:48 +0300

    create project

---
+++ 06.Migrations-Exercise/AcademicRecordsApp/StudentsSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.AccessControl;
+using StudentsSystem.Data;
+using StudentsSystem.Data.Models;
+using ResourceType = StudentsSystem.Data.Models.ResourceType;
+
+namespace StudentsSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new StudentSystemDbContext();
+
+            var resource = new Resource()
+            {
+                Name = "EF Into video",
+                Url = "https://softuni.bg/ef/video1",
+                ResourceType = ResourceType.Video,
+            };
+
+            //  context.Resources.Add(resource);
+            // context.SaveChanges();
+
+            var resources = context.Resources.ToArray();
+
+            foreach (var r in resources)
+            {
+                Console.WriteLine($"{r.Name} {r.ResourceType}");
+            }
+        }
+    }
+}
+
