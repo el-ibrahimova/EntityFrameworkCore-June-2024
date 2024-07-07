1a9b97824bfdb81cea2b82644c88e3438cc790f6
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-07 11:47:12 +0300

    create DbContext

---
+++ 06.Migrations-Exercise/AcademicRecordsApp/StudentsSystem/Data/StudentSystemDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsSystem.Data.Models;
+
+namespace StudentsSystem.Data
+{
+    public class StudentSystemDbContext:DbContext
+    {
+        public StudentSystemDbContext()
+        {
+        }
+
+        public StudentSystemDbContext(DbContextOptions<StudentSystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Resource> Resources { get; set; } = null!;
+        public virtual DbSet<Course> Courses { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        public virtual DbSet<StudentCourse> StudentsCourses { get; set; } = null!;
+ 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=DESKTOP-SENJ7PO\\SQLEXPRESS;Database=StudentSystemDB;Integrated Security=True;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentsCourses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentsCourses)
+                .HasForeignKey(sc => sc.CourseId);
+        }
+
+    }
+}
