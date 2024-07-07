775bf1caaea6d28f8bea15f047af66196e09ee32
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-07 11:44:56 +0300

    add models

---
+++ 06.Migrations-Exercise/AcademicRecordsApp/AcademicRecordsApp/Data/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AcademicRecordsApp.Data.Models
+{
+    public partial class Course
+    {
+        public Course()
+        {
+            Students = new HashSet<Student>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
