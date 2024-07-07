1056b350e35ec06e7d6270918948b4ebfc29b949
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-07 11:46:58 +0300

    add enumeration class

---
+++ 06.Migrations-Exercise/AcademicRecordsApp/StudentsSystem/Data/Models/ResourceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsSystem.Data.Models
+{
+    public enum ResourceType
+    {
+        Video,
+        Presentation,
+        Document,
+        Other
+    }
+}
+
