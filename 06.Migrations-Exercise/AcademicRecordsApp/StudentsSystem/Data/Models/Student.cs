245b525a026396bbd2717692607b872c1d0ecad3
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-07 11:46:48 +0300

    add models

---
+++ 06.Migrations-Exercise/AcademicRecordsApp/StudentsSystem/Data/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentsSystem.Data.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        public Student()
+        {
+            StudentsCourses = new HashSet<StudentCourse>();
+        }
+
+        [Key]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public DateTime? Birthday { get; set; }
+
+        public ICollection<StudentCourse> StudentsCourses { get; set; }
+    }
+}
---
+++ 06.Migrations-Exercise/AcademicRecordsApp/StudentsSystem/Data/Models/StudentCourse.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentsSystem.Data.Models
+{
+    [Table("StudentsCourses")]
+    public class StudentCourse
+    {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
