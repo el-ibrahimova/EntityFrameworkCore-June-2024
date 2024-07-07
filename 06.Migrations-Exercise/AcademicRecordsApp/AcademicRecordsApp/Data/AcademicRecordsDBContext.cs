6c610917d7ff8e24395245be7016b7472b055a65
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-07 11:45:12 +0300

    create DbContext

---
+++ 06.Migrations-Exercise/AcademicRecordsApp/AcademicRecordsApp/Data/AcademicRecordsDBContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using AcademicRecordsApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AcademicRecordsApp.Data
+{
+    public partial class AcademicRecordsDBContext : DbContext
+    {
+        public AcademicRecordsDBContext()
+        {
+        }
+
+        public AcademicRecordsDBContext(DbContextOptions<AcademicRecordsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Courses { get; set; } = null!;
+        public virtual DbSet<Exam> Exams { get; set; } = null!;
+        public virtual DbSet<Grade> Grades { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-SENJ7PO\\SQLEXPRESS;Database=AcademicRecordsDB;Integrated Security=True;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.HasMany(d => d.Students)
+                    .WithMany(p => p.Courses)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "StudentsCourse",
+                        l => l.HasOne<Student>().WithMany().HasForeignKey("StudentsId"),
+                        r => r.HasOne<Course>().WithMany().HasForeignKey("CoursesId"),
+                        j =>
+                        {
+                            j.HasKey("CoursesId", "StudentsId");
+
+                            j.ToTable("StudentsCourses");
+
+                            j.HasIndex(new[] { "StudentsId" }, "IX_StudentsCourses_StudentsId");
+                        });
+            });
+
+            modelBuilder.Entity<Exam>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Grade>(entity =>
+            {
+                entity.HasIndex(e => e.ExamId, "IX_Grades_ExamId");
+
+                entity.HasIndex(e => e.StudentId, "IX_Grades_StudentId");
+
+                entity.Property(e => e.Value).HasColumnType("decimal(3, 2)");
+
+                entity.HasOne(d => d.Exam)
+                    .WithMany(p => p.Grades)
+                    .HasForeignKey(d => d.ExamId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Grades_Exams");
+
+                entity.HasOne(d => d.Student)
+                    .WithMany(p => p.Grades)
+                    .HasForeignKey(d => d.StudentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Grades_Students");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.FullName).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
