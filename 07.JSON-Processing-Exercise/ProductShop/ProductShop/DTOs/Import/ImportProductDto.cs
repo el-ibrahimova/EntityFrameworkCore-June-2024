d528a4758286cec81c37c4be271f2f7d1a4eb1f2
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-08 12:32:19 +0300

    create DTO classes

---
+++ 07.JSON-Processing-Exercise/ProductShop/ProductShop/DTOs/Import/ImportProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductShop.DTOs.Import
+{
+    public class ImportProductDto
+    {
+        // here we don't need to add attributes for the names of properties, because they are the same convention as in C#
+
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int SellerId { get; set; }
+
+        public int? BuyerId { get; set; }
+    }
+}
