3a2a73c795cb470dc50375140af9724b10d2e26c
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-09 13:22:03 +0300

    added data and datasets

---
+++ 07.JSON-Processing-Exercise/ProductShop/ProductShop/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+           @"Server=DESKTOP-SENJ7PO\SQLEXPRESS;Database=ProductShop;Integrated Security=True";
+
+    }
+}
