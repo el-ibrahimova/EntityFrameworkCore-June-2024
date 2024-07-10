5069416d24ebdabab00939a5e356b8436448724a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-10 16:35:48 +0300

    create dto for customer total sales

---
+++ 07.JSON-Processing-Exercise/CarDealer/CarDealer/DTOs/Export/CustomerTotalSalesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarDealer.DTOs.Export
+{
+    public class CustomerTotalSalesDto
+    {
+        public string FullName { get; set; }
+        public int BoughtCars { get; set; }
+        public decimal SpentMoney { get; set; }
+    }
+}
