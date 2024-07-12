ddf599d5cae78410fecd3d6de52f23b495c8ff24
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-12 11:31:08 +0300

    write logic for json serializer for method ImportSellers

---
+++ Exam-Preparation/Boardgames - 01 April 2023/Boardgames/DataProcessor/Deserializer.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using Boardgames.Data.Models;
+using Boardgames.Data.Models.Enums;
+using Boardgames.DataProcessor.ImportDto;
+using Newtonsoft.Json;
+
+namespace Boardgames.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+    using Boardgames.Data;
+   
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCreator
+            = "Successfully imported creator – {0} {1} with {2} boardgames.";
+
+        private const string SuccessfullyImportedSeller
+            = "Successfully imported seller - {0} with {1} boardgames.";
+
+        public static string ImportCreators(BoardgamesContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportSellers(BoardgamesContext context, string jsonString)
+        {
+            StringBuilder sb = new();
+            var sellerDtos = JsonConvert.DeserializeObject<ImportSellerDto[]>(jsonString);
+
+            HashSet<Seller> sellerList = new();
+
+            var uniqueBoradgameIds = context.Boardgames
+                .Select(bg => bg.Id)
+                .ToArray();
+
+            foreach (ImportSellerDto sellerDto in sellerDtos)
+            {
+                if (!IsValid(sellerDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Seller seller = new()
+                {
+                    Name = sellerDto.Name,
+                    Address = sellerDto.Address,
+                    Country = sellerDto.Country,
+                    Website = sellerDto.Website
+                };
+
+                foreach (var baordgameId in sellerDto.BoardgamesIds.Distinct())
+                {
+                    if (!uniqueBoradgameIds.Contains(baordgameId))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    BoardgameSeller bgs = new()
+                    {
+                        Seller = seller,
+                        BoardgameId = baordgameId
+                    };
+
+                    seller.BoardgamesSellers.Add(bgs);
+                }
+
+                sellerList.Add(seller);
+                sb.AppendLine(string.Format(SuccessfullyImportedSeller, seller.Name, seller.BoardgamesSellers.Count()));
+            }
+
+            context.AddRange(sellerList);
+            context.SaveChanges();
+
+            return sb.ToString();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
