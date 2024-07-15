cbc0d2371d7fcc0daec8738e7a1260612bfb5ea2
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-15 16:25:09 +0300

    create export DTOs

---
+++ 08.XML-Processing-Exercises/CarDealer/CarDealer/DTOs/Export/CarWithPartsDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+using CarDealer.Models;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("car")]
+    public class CarWithPartsDto
+    {
+        [XmlAttribute("make")]
+        public string Make { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+
+        [XmlAttribute("traveled-distance")]
+        public long TraveledDistance { get; set; }
+
+        [XmlArray("parts")]
+        public PartsForCarsDto[] Parts { get; set; }
+    }
+
+    
+}
