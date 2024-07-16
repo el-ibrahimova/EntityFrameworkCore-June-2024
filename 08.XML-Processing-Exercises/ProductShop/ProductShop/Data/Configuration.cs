13ac8c6bc435ca190157cc52877ed59999b696f5
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-16 09:25:22 +0300

    create project

---
+++ 08.XML-Processing-Exercises/ProductShop/ProductShop/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = "Server=DESKTOP-SENJ7PO\\SQLEXPRESS;Database=ProductShop;Integrated Security=True";
+    }
+}
