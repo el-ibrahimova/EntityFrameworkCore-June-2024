7ebcdff6aafdbacea6df6431c23ba6e19ca0efdb
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-18 11:26:05 +0300

    create export Dto

---
+++ Exam-Preparation/Boardgames - 01 April 2023/Boardgames/DataProcessor/ExportDto/ExportCreatorsDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto
+{
+    [XmlType("Creator")]
+    public class ExportCreatorsDto
+    {
+        [XmlElement("CreatorName")]
+        public string CreatorName { get; set; }
+
+        [XmlArray("Boardgames")]
+        public BoardgamesDto[] Boardgames { get; set; }
+
+        [XmlAttribute("BoardgamesCount")]
+        public int BoardgamesCount { get; set; }
+    }
+}
