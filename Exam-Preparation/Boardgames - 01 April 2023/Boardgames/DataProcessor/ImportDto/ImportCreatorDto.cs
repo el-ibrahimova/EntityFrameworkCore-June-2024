4d7104299be1a790c07cdc65956a1af1f852466f
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-18 11:26:33 +0300

    create ImportDto

---
+++ Exam-Preparation/Boardgames - 01 April 2023/Boardgames/DataProcessor/ImportDto/ImportCreatorDto.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Boardgames.Common;
+using Boardgames.Data.Models.Enums;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    [XmlType("Creator")]
+    public class ImportCreatorDto
+    {
+
+        [Required]
+        [MinLength(ValidationConstants.CreatorNameMinLength)]
+        [MaxLength(ValidationConstants.CreatorNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(ValidationConstants.CreatorNameMinLength)]
+        [MaxLength(ValidationConstants.CreatorNameMaxLength)]
+        public string LastName { get; set; }
+
+        [XmlArray("Boardgames")]
+        public BoardgameDto[] Boardgames { get; set; }
+
+    }
+
+    [XmlType("Boardgame")]
+    public class BoardgameDto
+    {
+        [Required]
+        [MinLength(ValidationConstants.BoardgameNameMinLength)]
+        [MaxLength(ValidationConstants.BoardgameNameMaxLength)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(ValidationConstants.BoardgameMinRating, ValidationConstants.BoardgameMaxRating)]
+        [XmlElement("Rating")]
+        public double Rating { get; set; }
+
+        [XmlElement("CategoryType")]
+        public int CategoryType { get; set; }
+
+        [Required]
+        [Range(ValidationConstants.BoardgameMinYearPublished, ValidationConstants.BoardgameMaxYearPublished)]
+        [XmlElement("YearPublished")]
+        public int YearPublished { get; set; }
+
+        [Required]
+        [XmlElement("Mechanics")]
+        public string Mechanics { get; set; }
+    }
+}
