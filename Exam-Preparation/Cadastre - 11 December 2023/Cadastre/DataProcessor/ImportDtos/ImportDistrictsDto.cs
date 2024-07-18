437a3b478ebc679c96503312e8329433e793a25e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-18 11:28:20 +0300

    create ImportDto

---
+++ Exam-Preparation/Cadastre - 11 December 2023/Cadastre/DataProcessor/ImportDtos/ImportDistrictsDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Cadastre.Common;
+using Cadastre.Data.Enumerations;
+using static Cadastre.Common.ValidationConstants;
+
+namespace Cadastre.DataProcessor.ImportDtos
+{
+    [XmlType("District")]
+    public class ImportDistrictsDto
+    {
+
+        [XmlAttribute("Region")]
+        [EnumDataType(typeof(Region))]
+        [Required]
+        public string Region { get; set; } = null!;
+
+
+        [MinLength(DistrictNameMinLength)]
+        [MaxLength(DistrictNameMaxLength)]
+        [XmlElement("Name")]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [MinLength(DistrictPostalCodeMinLength)]
+        [MaxLength(DistrictNameMaxLength)]
+        [XmlElement("PostalCode")]
+        [Required]
+        public string PostalCode { get; set; } = null!;
+
+        [XmlArray("Properties")]
+        public ImportPropertyDto[] Properties { get; set; } = null!;
+    }
+}
---
+++ Exam-Preparation/Cadastre - 11 December 2023/Cadastre/DataProcessor/ImportDtos/ImportPropertyDto.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+using static Cadastre.Common.ValidationConstants;
+
+namespace Cadastre.DataProcessor.ImportDtos
+
+{
+
+    [XmlType("Property")]
+    public class ImportPropertyDto
+    {
+        [MinLength(PropertyIdentifierMinLength)]
+        [MaxLength(PropertyIdentifierMaxLength)]
+        [Required]
+        [XmlElement("PropertyIdentifier")]
+        public string PropertyIdentifier { get; set; } = null!;
+
+        [Range(PropertyAreaMinValue, PropertyAreaMaxValue)]
+        [Required]
+        [XmlElement("Area")]
+        public int Area { get; set; }
+
+        [XmlElement("Details")]
+        [MinLength(PropertyDetailsMinLength)]
+        [MaxLength(PropertyDetailsMaxLength)]
+        public string? Details { get; set; }
+
+        [XmlElement("Address")]
+        [MinLength(PropertyAddressMinLength)]
+        [MaxLength(PropertyAddressMaxLength)]
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [XmlElement("DateOfAcquisition")]
+        [Required]
+        public string DateOfAcquisition { get; set; }
+    }
+}
