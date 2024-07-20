e1370d683d1c31966b0ddd1abeae57e97ca29412
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-20 16:00:48 +0300

    create models

---
+++ Exam-Preparation/Invoices - 11 April 2023/Invoices/Data/Models/Address.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Invoices.Common;
+using static Invoices.Common.ValidationConstants;
+
+namespace Invoices.Data.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(AddressStreetNameMaxLength)]
+        public string StreetName { get; set; } = null!;
+
+        [Required]
+        public int StreetNumber { get; set; }
+
+        [Required] 
+        [MaxLength(AddressPostCodeMaxLength)] 
+        public string PostCode { get; set; } = null!;
+
+        [Required]
+        [MaxLength(AddressCityMaxLength)]
+        public string City { get; set; } = null!;
+
+        [Required]
+        [MaxLength(AddressCountryMaxLength)]
+        public string Country { get; set; } = null!;
+
+     
+        [ForeignKey(nameof(Client))]
+        public int ClientId { get; set; }
+
+        public virtual Client Client { get; set; } = null!;
+
+    }
+}
---
+++ Exam-Preparation/Invoices - 11 April 2023/Invoices/Data/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Invoices.Common;
+using Invoices.Data.Models.Enums;
+
+namespace Invoices.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            this.ProductsClients = new HashSet<ProductClient>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ValidationConstants.ProducNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        public virtual ICollection<ProductClient> ProductsClients { get; set; } = null!;
+    }
+}
