d58a322f540b0278d71b126cbee40bd39dce005a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-21 16:13:15 +0300

    create configuration

---
+++ Exam-Preparation/Trucks - 15 August 2022/Trucks/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Trucks.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = "Server=DESKTOP-SENJ7PO\\SQLEXPRESS;Database=Trucks;Integrated Security=True";
+    }
+}
---
+++ Exam-Preparation/Trucks - 15 August 2022/Trucks/Data/TrucksContext.cs
@@ -0,0 +1,40 @@
+﻿using Trucks.Data.Models;
+
+namespace Trucks.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext()
+        { 
+        }
+
+        public TrucksContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        public DbSet<Client> Clients { get; set; } = null!;
+        public DbSet<ClientTruck> ClientsTrucks { get; set; } = null!;
+        public DbSet<Despatcher> Despatchers { get; set; } = null!;
+        public DbSet<Truck> Trucks { get; set; } = null!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClientTruck>(e =>
+                e.HasKey(c => new {c.ClientId, c.TruckId}));
+        }
+    }
+}
