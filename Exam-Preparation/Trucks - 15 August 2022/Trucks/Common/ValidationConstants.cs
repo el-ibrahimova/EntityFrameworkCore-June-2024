f72b052fc9de6ed44fda73981b5fe0ce67bec6c5
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-21 16:13:33 +0300

    create validation constants

---
+++ Exam-Preparation/Trucks - 15 August 2022/Trucks/Common/ValidationConstants.cs
@@ -0,0 +1,33 @@
+﻿namespace Trucks.Common
+{
+    public static class ValidationConstants
+    {
+        // Truck
+        public const string TruckRegistrationNumberRegEx = @"^[A-Z]{2}\d{4}[A-Z]{2}$";
+        public const int TruckVinNumberMinLength = 17;
+        public const int TruckVinNumberMaxLength = 17;
+        public const int TruckTankCapacityMinValue = 950;
+        public const int TruckTankCapacityMaxValue = 1420;
+        public const int TruckCargoCapacityMinValue = 5000;
+        public const int TruckCargoCapacityMaxValue = 29000;
+        public const int TruckCategoryTypeMinValue = 0;
+        public const int TruckCategoryTypeMaxValue = 3;
+        public const int TruckMakeTypeMinValue = 0;
+        public const int TruckMakeTypeMaxValue = 4;
+
+        // Client
+        public const int ClientNameMinLength = 3;
+        public const int ClientNameMaxLength = 40;
+        public const int ClientNationalityMinValue = 2;
+        public const int ClientNationalityMaxValue = 40;
+        public const int ClientTypeMinLength = 0;
+        public const int ClientTypeMaxLength = int.MaxValue;
+
+        // Despatcher
+
+        public const int DespatcherNameMinLength = 2;
+        public const int DespatcherNameMaxLength = 40;
+        public const int DespatcherPositionMinLength = 0;
+        public const int DespatcherPositionMaxLength = int.MaxValue;
+    }
+}
