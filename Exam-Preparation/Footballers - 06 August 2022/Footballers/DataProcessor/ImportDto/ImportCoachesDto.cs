3a15528ed43491551de037f9bef5492968aaa33e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-22 22:04:29 +0300

    create Import Dto for Import Coaches Method

---
+++ Exam-Preparation/Footballers - 06 August 2022/Footballers/DataProcessor/ImportDto/ImportCoachesDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class ImportCoachesDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [XmlElement("Nationality")]
+      public string Nationality { get; set; } = null!;
+     
+        [XmlArray("Footballers")] 
+        public ImportFootballersDto[] Footballers { get; set; } = null!;
+    }
+}
