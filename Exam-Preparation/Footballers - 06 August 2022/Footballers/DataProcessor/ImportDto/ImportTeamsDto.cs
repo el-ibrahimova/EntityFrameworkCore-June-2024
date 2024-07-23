7079414c4fa51f56dff432a6fa1764c8a72fcd02
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-23 20:28:28 +0300

    create ImportDto

---
+++ Exam-Preparation/Footballers - 06 August 2022/Footballers/DataProcessor/ImportDto/ImportTeamsDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class ImportTeamsDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        [RegularExpression( @"^[A-Za-z0-9\s\.\-]{3,}$")]
+       public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+   public string Nationality { get; set; } = null!;
+        
+        [Required]
+      public int Trophies { get; set; }
+
+        [JsonProperty("Footballers")]
+        public int[] Footballes { get; set; } 
+    }
+}
