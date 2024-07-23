709455af519e34fb5aacb8b4b1a0b3f6ec636bb7
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-23 20:53:08 +0300

    create export Dtos

---
+++ Exam-Preparation/Footballers - 06 August 2022/Footballers/DataProcessor/ExportDto/ExportCoachWithFootballersDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Coach")]
+    public class ExportCoachWithFootballersDto
+    {
+        [XmlAttribute]
+        public int FootballersCount { get; set; }
+
+        public string CoachName { get; set; }
+        
+        [XmlArray]
+        public ExportFootballersDto[] Footballers { get; set; }
+    }
+}
---
+++ Exam-Preparation/Footballers - 06 August 2022/Footballers/DataProcessor/ExportDto/ExportFootballersDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+using Footballers.Data.Models;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Footballer")]
+    public class ExportFootballersDto
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+}
