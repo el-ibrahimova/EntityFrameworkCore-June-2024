58dfb19bc5ac93fc489f28a0834acfba3c1a852d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-25 22:56:29 +0300

    create Models

---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/Data/Models/Gun.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Artillery.Data.Models.Enums;
+
+namespace Artillery.Data.Models
+{
+    public class Gun
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Manufacturer))]
+        public int ManufacturerId { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; } = null!;
+
+        [Required]
+        public int GunWeight { get; set; }
+
+        [Required]
+        public double BarrelLength { get; set; }
+
+        public int? NumberBuild { get; set; }
+
+        [Required]
+        public int Range { get; set; }
+
+        [Required]
+        public GunType GunType { get; set; }
+
+        [ForeignKey(nameof(Shell))]
+        public int ShellId { get; set; }
+        public virtual Shell Shell { get; set; } = null!;
+
+
+        public virtual ICollection<CountryGun> CountriesGuns { get; set; } = new HashSet<CountryGun>();
+
+
+
+    }
+}
---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/Data/Models/Shell.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models
+{
+    public class Shell
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public double ShellWeight { get; set; }
+
+        [Required]
+        [MaxLength(ValidationConstants.ShellCaliberMaxLength)]
+        public string Caliber { get; set; } = null!;
+
+        public virtual ICollection<Gun> Guns { get; set; } = new HashSet<Gun>();
+    }
+}
