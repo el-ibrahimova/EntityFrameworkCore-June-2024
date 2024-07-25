8acca0bf2b2242e97ed35d922140faae1f0bd8fb
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-25 22:57:26 +0300

    create import dtos for country and manufacturer

---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/DataProcessor/ImportDto/ImportCountryDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Artillery.Data;
+using Artillery.Data.Models;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType(nameof(Country))]
+    public class ImportCountryDto
+    {
+        [Required]
+        [XmlElement(nameof(CountryName))]
+        [MaxLength(ValidationConstants.CountryNameMaxLength)]
+        [MinLength(ValidationConstants.CountryNameMinLength)]
+        public string CountryName { get; set; } = null!;
+        
+
+        [Required]
+        [XmlElement(nameof(ArmySize))]
+        [Range(ValidationConstants.CountryArmySizeMinValue, ValidationConstants.CountryArmySizeMaxValue)]
+        public int ArmySize { get; set; }
+    }
+}
---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/DataProcessor/ImportDto/ImportManufacturerDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Artillery.Data;
+using Artillery.Data.Models;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType(nameof(Manufacturer))]
+    public class ImportManufacturerDto
+    {
+        [Required]
+        [XmlElement(nameof(ManufacturerName))]
+        [MinLength(ValidationConstants.ManufacturerNameMinLength)]
+        [MaxLength(ValidationConstants.ManufacturerNameMaxLength)]
+        public string ManufacturerName { get; set; } = null!;
+
+        [Required]
+        [XmlElement(nameof(Founded))]
+        [MinLength(ValidationConstants.ManufacturerFoundedMinLength)]
+        [MaxLength(ValidationConstants.ManufacturerFoundedMaxLength)]
+        public string Founded { get; set; } = null!;
+
+    }
+}
