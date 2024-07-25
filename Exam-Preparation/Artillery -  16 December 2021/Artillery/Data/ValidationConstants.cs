931bd23c5f7beefc9b33cb7a1b401b50280c5a39
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-25 22:56:09 +0300

    create validation constants

---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/Data/ValidationConstants.cs
@@ -0,0 +1,32 @@
+﻿namespace Artillery.Data
+{
+    public static class ValidationConstants
+    {
+        // Country 
+        public const int CountryNameMinLength = 4;
+        public const int CountryNameMaxLength = 60;
+        public const int CountryArmySizeMinValue = 50_000;
+        public const int CountryArmySizeMaxValue = 10_000_000;
+
+        // Shell
+        public const string ShellWeightMinValue = "2";
+        public const string ShellWeightMaxValue = "1_680";
+        public const int ShellCaliberMinLength = 4;
+        public const int ShellCaliberMaxLength = 30;
+
+        // Gun
+        public const int GunWeightMinValue = 100;
+        public const int GunWeightMaxValue = 1_350_000;
+        public const string GunBarrelMinValue = "2.00";
+        public const string GunBarrelMaxValue = "35.00";
+        public const int GunRangeMinValue = 1;
+        public const int GunRangeMaxValue = 100_000;
+
+        // Manufacturer
+        public const int ManufacturerNameMinLength = 4;
+        public const int ManufacturerNameMaxLength = 40;
+        public const int ManufacturerFoundedMinLength = 10;
+        public const int ManufacturerFoundedMaxLength = 100;
+
+    }
+}
