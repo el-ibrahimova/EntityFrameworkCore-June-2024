8c7574f41e5344bb1cc700a1c96bcc83e4f57b27
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-26 23:54:52 +0300

    create dto

---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/DataProcessor/ImportDto/ImportGunDto.cs
@@ -0,0 +1,38 @@
+﻿using Artillery.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    public class ImportGunDto
+    {
+        public int ManufacturerId { get; set; }
+
+        [Required]
+        [Range(ValidationConstants.GunWeightMinValue, ValidationConstants.GunWeightMaxValue)]
+        public int GunWeight { get; set; }
+
+        [Required]
+        [Range(typeof(double), ValidationConstants.GunBarrelMinValue, ValidationConstants.GunBarrelMaxValue)]
+        public double BarrelLength { get; set; }
+
+
+        public int? NumberBuild { get; set; }
+
+        [Required]
+        [Range(ValidationConstants.GunRangeMinValue, ValidationConstants.GunRangeMaxValue)]
+        public int Range { get; set; }
+
+        // it throws error when executing program with this code
+        // [Required]
+        // [Range(ValidationConstants.GunTypeMinValue, ValidationConstants.GunTypeMaxValue)]
+        // public string GunType { get; set; } = null!;
+
+        [Required]
+        public string GunType { get; set; } = null!;
+
+        [Required]
+        public int ShellId { get; set; }
+
+        public ImportGunCountriesDto[] Countries { get; set; }
+    }
+}
