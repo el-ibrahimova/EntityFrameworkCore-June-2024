aa925420b9b0c959f966bffc4d83d2f3357f39fb
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-26 23:55:01 +0300

    create dto

---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/DataProcessor/ImportDto/ImportShellDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Artillery.Data;
+using Artillery.Data.Models;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType(nameof(Shell))]
+    public class ImportShellDto
+    {
+        [Required]
+        [XmlElement(nameof(ShellWeight))]
+        [Range(typeof(double),ValidationConstants.ShellWeightMinValue, ValidationConstants.ShellWeightMaxValue)]
+        public double ShellWeight { get; set; }
+
+        [Required]
+        [MinLength(ValidationConstants.ShellCaliberMinLength)]
+        [MaxLength(ValidationConstants.ShellCaliberMaxLength)]
+        [XmlElement(nameof(Caliber))]
+        public string Caliber { get; set; } = null!;
+    }
+}
