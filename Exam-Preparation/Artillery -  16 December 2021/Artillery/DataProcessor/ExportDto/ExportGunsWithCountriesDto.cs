0e4b55970edb515c65a820cc1eae150e67a8c50b
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-27 22:25:34 +0300

    create dtos for ExportGuns method

---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/DataProcessor/ExportDto/ExportGunsWithCountriesDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+using Artillery.Data.Models;
+
+namespace Artillery.DataProcessor.ExportDto
+{
+    [XmlType(nameof(Gun))]
+    public class ExportGunsWithCountriesDto
+    {
+        [XmlAttribute(nameof(Manufacturer))] public string Manufacturer { get; set; } = null!;
+
+        [XmlAttribute(nameof(GunType))]
+        public string GunType { get; set; }
+
+
+        [XmlAttribute(nameof(GunWeight))]
+        public int GunWeight { get; set; }
+
+        [XmlAttribute(nameof(BarrelLength))]
+        public double BarrelLength { get; set; }
+
+        [XmlAttribute(nameof(Range))]
+        public int Range { get; set; }
+
+
+        [XmlArray(nameof(Countries))] public ExportCountriesDto[] Countries { get; set; } = null!;
+    }
+}
