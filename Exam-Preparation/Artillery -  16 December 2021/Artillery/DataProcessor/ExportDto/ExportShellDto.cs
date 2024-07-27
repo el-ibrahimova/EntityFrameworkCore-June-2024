394358e4369dead26bc3fb2f4b402c2d85f20632
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-27 22:26:02 +0300

    create dtos for ExportShells method

---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/DataProcessor/ExportDto/ExportShellDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Artillery.DataProcessor.ExportDto
+{
+    public class ExportShellDto
+    {
+        public double ShellWeight { get; set; }
+        public string Caliber { get; set; } = null!;
+        public ExportShellGunDto[] Guns { get; set; }= null!;
+    }
+}
---
+++ Exam-Preparation/Artillery -  16 December 2021/Artillery/DataProcessor/ExportDto/ExportShellGunDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Artillery.DataProcessor.ExportDto
+{
+    public class ExportShellGunDto
+    {
+        public string GunType { get; set; } = null!;
+        public int GunWeight { get; set; }
+        public double BarrelLength { get; set; }
+        public string Range { get; set; } 
+    }
+}
