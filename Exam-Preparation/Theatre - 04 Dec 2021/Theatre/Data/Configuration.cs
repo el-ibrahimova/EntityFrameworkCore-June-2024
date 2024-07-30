ce23aa41ce5cbb37883560b6c4debeac7fad11dc
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-30 22:12:03 +0300

    create context and configuration

---
+++ Exam-Preparation/Theatre - 04 Dec 2021/Theatre/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Theatre.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = "Server=DESKTOP-SENJ7PO\\SQLEXPRESS;Database=Theatre;Integrated Security=True";
+    }
+}
---
+++ Exam-Preparation/Theatre - 04 Dec 2021/Theatre/Data/TheatreContext.cs
@@ -0,0 +1,40 @@
+﻿
+using Theatre.Data.Models;
+
+namespace Theatre.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TheatreContext : DbContext
+    {
+        public TheatreContext() 
+        {
+        }
+
+        public TheatreContext(DbContextOptions options)
+        : base(options) 
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        public DbSet<Ticket> Tickets { get; set; } = null!;
+        public DbSet<Cast> Casts { get; set; } = null!;
+        public DbSet<Play> Plays { get; set; } = null!;
+        public DbSet<Models.Theatre> Theatres { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>()
+                .HasKey(t => new { t.TheatreId, t.PlayId });
+        }
+    }
+}
