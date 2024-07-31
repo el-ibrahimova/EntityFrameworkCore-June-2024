45f75601e349c582b41a5fbcee8a67cf3d7aaddc
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-07-31 23:10:18 +0300

    write logic for import results. there is a problem with ImportTtheatersTickets method - duplicated primary key

---
+++ Exam-Preparation/Theatre - 04 Dec 2021/Theatre/DataProcessor/Deserializer.cs
@@ -0,0 +1,185 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using Theatre.Data;
+using Theatre.Data.Models;
+using Theatre.Data.Models.Enums;
+using Theatre.DataProcessor.ImportDto;
+
+namespace Theatre.DataProcessor
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+
+    using Theatre.Data;
+    using Theatre.DataProcessor.ImportDto;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfulImportPlay
+            = "Successfully imported {0} with genre {1} and a rating of {2}!";
+
+        private const string SuccessfulImportActor
+            = "Successfully imported actor {0} as a {1} character!";
+
+        private const string SuccessfulImportTheatre
+            = "Successfully imported theatre {0} with #{1} tickets!";
+
+
+        public static string ImportPlays(TheatreContext context, string xmlString)
+        {
+            XmlRootAttribute root = new XmlRootAttribute("Plays");
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportPlaysDto[]), root);
+
+            using StringReader reader = new StringReader(xmlString);
+
+            var playsDtos = (ImportPlaysDto[])serializer.Deserialize(reader);
+
+            List<Play> validPlays = new();
+            StringBuilder sb = new();
+
+            var validGenres = new string[] { "Drama", "Comedy", "Romance", "Musical" };
+            TimeSpan minTime = new TimeSpan(1, 0, 0);
+
+            foreach (var playDto in playsDtos)
+            {
+                TimeSpan durationTime = TimeSpan.ParseExact(playDto.Duration, "c", CultureInfo.InvariantCulture);
+
+                if (!IsValid(playDto)
+                    || !validGenres.Contains(playDto.Genre)
+                    || (durationTime < minTime))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Play play = new Play()
+                {
+                    Title = playDto.Title,
+                    Duration = TimeSpan.ParseExact(playDto.Duration, "c", CultureInfo.InvariantCulture),
+                    Rating = playDto.Rating,
+                    Genre = (Genre)Enum.Parse(typeof(Genre), playDto.Genre),
+                    Description = playDto.Description,
+                    Screenwriter = playDto.Screenwriter
+                };
+
+                validPlays.Add(play);
+
+                sb.AppendLine(string.Format(SuccessfulImportPlay, play.Title, play.Genre, play.Rating));
+            }
+
+            context.AddRange(validPlays);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportCasts(TheatreContext context, string xmlString)
+        {
+            XmlRootAttribute root = new XmlRootAttribute("Casts");
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportCastsDto[]), root);
+
+            using StringReader reader = new StringReader(xmlString);
+
+            var castsDtos = (ImportCastsDto[])serializer.Deserialize(reader);
+
+            List<Cast> validCasts = new List<Cast>();
+            StringBuilder sb = new();
+
+
+            foreach (var castDto in castsDtos)
+            {
+                if (!IsValid(castDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Cast cast = new Cast()
+                {
+                    FullName = castDto.FullName,
+                    IsMainCharacter = castDto.IsMainCharacter,
+                    PhoneNumber = castDto.PhoneNumber,
+                    PlayId = castDto.PlayId  // player will always be valid => no need to validate data
+                };
+
+                validCasts.Add(cast);
+
+                string role = cast.IsMainCharacter == true ? "main" : "lesser";
+                sb.AppendLine(string.Format(SuccessfulImportActor, cast.FullName, role));
+            }
+
+            context.Casts.AddRange(validCasts);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportTtheatersTickets(TheatreContext context, string jsonString)
+        {
+            var theaterDtos = JsonConvert.DeserializeObject<ImportTheatresDto[]>(jsonString);
+
+            StringBuilder sb = new StringBuilder();
+            List<Data.Models.Theatre> validTheatres = new List<Data.Models.Theatre>();
+
+            foreach (var theatreDto in theaterDtos)
+            {
+                if (!IsValid(theatreDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+             List<Ticket> validTickets = new List<Ticket>();
+
+                foreach (var ticketDto in theatreDto.Tickets)
+                {
+                    if (!IsValid(ticketDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Ticket ticket = new Ticket()
+                    {
+                        PlayId = ticketDto.PlayId,
+                        Price = ticketDto.Price,
+                        RowNumber = ticketDto.RowNumber
+                    };
+                    validTickets.Add(ticket);
+                }
+
+                Data.Models.Theatre theatre = new Data.Models.Theatre()
+                {
+                    Director = theatreDto.Director,
+                    Name = theatreDto.Name,
+                    NumberOfHalls = theatreDto.NumberOfHalls,
+                    Tickets = validTickets
+                };
+                validTheatres.Add(theatre);
+                sb.AppendLine(String.Format(SuccessfulImportTheatre, theatre.Name, theatre.Tickets.Count));
+            }
+            
+            context.Theatres.AddRange(validTheatres);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+            
+            // visual studio throws exception for this method for duplicated primary key when executing, but it is true in judge
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validator = new ValidationContext(obj);
+            var validationRes = new List<ValidationResult>();
+
+            var result = Validator.TryValidateObject(obj, validator, validationRes, true);
+            return result;
+        }
+    }
+}
