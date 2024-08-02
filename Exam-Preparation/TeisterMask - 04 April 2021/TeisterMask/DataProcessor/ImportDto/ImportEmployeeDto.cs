4718c8a1267e68632c7cfcd01138e2b9b1215efa
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-02 13:27:02 +0300

    import dtos

---
+++ Exam-Preparation/TeisterMask - 04 April 2021/TeisterMask/DataProcessor/ImportDto/ImportEmployeeDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class ImportEmployeeDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        [RegularExpression(@"^[A-Za-z0-9]{3,}$")]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+        public string Phone { get; set; }
+        public int[] Tasks { get; set; }
+    }
+}
---
+++ Exam-Preparation/TeisterMask - 04 April 2021/TeisterMask/DataProcessor/ImportDto/ImportProjectsDto.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Project")]
+    public class ImportProjectsDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("OpenDate")]
+        [Required]
+        public string OpenDate { get; set; } = null!;
+
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; }
+
+        [XmlArray("Tasks")]
+        public ExportTasksDto[] Tasks { get; set; } = null!;
+
+    }
+
+    [XmlType("Task")]
+    public class ExportTasksDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("OpenDate")]
+        [Required]
+        public string OpenDate { get; set; } = null!;
+
+        [XmlElement("DueDate")] 
+        [Required] 
+        public string DueDate { get; set; } = null!;
+
+        [XmlElement("ExecutionType")]
+        [Required]
+        [Range(0,3)]
+        public int ExecutionType { get; set; }
+
+        [XmlElement("LabelType")]
+        [Required]
+        [Range(0,4)]
+        public int LabelType { get; set; }
+    }
+}
