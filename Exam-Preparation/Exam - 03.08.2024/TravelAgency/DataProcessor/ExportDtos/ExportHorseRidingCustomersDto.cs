c8b63a0d59f49adc52ee183b460cc7f2192ecd5a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-03 22:20:38 +0300

    create export dtos

---
+++ Exam-Preparation/Exam - 03.08.2024/TravelAgency/DataProcessor/ExportDtos/ExportHorseRidingCustomersDto.cs
@@ -0,0 +1,16 @@
+﻿namespace TravelAgency.DataProcessor.ExportDtos
+{
+    public class ExportHorseRidingCustomersDto
+    {
+       public string FullName { get; set; }
+       public string PhoneNumber { get; set; }
+       public ExportHorseRidingToursDto[] Bookings { get; set; }
+
+    }
+
+    public class ExportHorseRidingToursDto
+    {
+        public string TourPackageName { get; set; }
+        public string Date { get; set; }
+    }
+}
---
+++ Exam-Preparation/Exam - 03.08.2024/TravelAgency/DataProcessor/ExportDtos/ExportSpanishSpeakingGuidesDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Serialization;
+
+namespace TravelAgency.DataProcessor.ExportDtos
+{
+    [XmlType("Guide")]
+    public class ExportSpanishSpeakingGuidesDto
+    {
+        [XmlElement("FullName")]
+        public string FullName { get; set; }
+
+        [XmlArray("TourPackages")]
+        public ExportTourPackagesDto[] TourPackages { get; set; }
+
+    }
+
+    [XmlType("TourPackage")]
+    public class ExportTourPackagesDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Description")]
+        public string Description { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+    }
+}
