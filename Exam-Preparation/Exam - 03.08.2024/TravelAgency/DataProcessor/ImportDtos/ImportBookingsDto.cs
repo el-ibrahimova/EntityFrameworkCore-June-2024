2fdc9904738101d322209119e9eab86e40932ddd
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-03 22:20:23 +0300

    create import dtos

---
+++ Exam-Preparation/Exam - 03.08.2024/TravelAgency/DataProcessor/ImportDtos/ImportBookingsDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using TravelAgency.Data.Models;
+
+namespace TravelAgency.DataProcessor.ImportDtos
+{
+    public class ImportBookingsDto
+    {
+        [Required]
+        [JsonProperty("BookingDate")]
+        public string BookingDate { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(60)]
+        [JsonProperty("CustomerName")]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [JsonProperty("TourPackageName")]
+        public string TouPackageName { get; set; }
+    }
+}
---
+++ Exam-Preparation/Exam - 03.08.2024/TravelAgency/DataProcessor/ImportDtos/ImportCustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using TravelAgency.Data.Models;
+
+namespace TravelAgency.DataProcessor.ImportDtos
+{
+    [XmlType(nameof(Customer))]
+    public class ImportCustomerDto
+    {
+        [XmlAttribute("phoneNumber")]
+        [Required]
+        [RegularExpression(@"^\+\d{12}$")]
+        public string PhoneNumber { get; set; }
+
+        [XmlElement("FullName")]
+        [Required]
+        [MinLength(4)]
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+        [XmlElement("Email")]
+        [Required]
+        [MinLength(6)]
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
