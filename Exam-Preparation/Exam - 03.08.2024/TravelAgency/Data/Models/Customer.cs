7bb20e73fd41fed634e6cebc07154957fb2288e4
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-03 22:20:01 +0300

    create Models

---
+++ Exam-Preparation/Exam - 03.08.2024/TravelAgency/Data/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelAgency.Data.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Bookings = new HashSet<Booking>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(13)]
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
---
+++ Exam-Preparation/Exam - 03.08.2024/TravelAgency/Data/Models/Guide.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TravelAgency.Data.Models.Enums;
+
+namespace TravelAgency.Data.Models
+{
+    public class Guide
+    {
+        public Guide()
+        {
+            TourPackagesGuides = new HashSet<TourPackageGuide>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        public Language Language { get; set; }
+
+        public virtual ICollection<TourPackageGuide> TourPackagesGuides { get; set; }
+    }
+}
