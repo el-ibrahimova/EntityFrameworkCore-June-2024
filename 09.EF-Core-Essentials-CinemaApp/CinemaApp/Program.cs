1e9b0f6008b98c2febfab04fc93d7743e07845a2
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-05 22:55:59 +0300

    create project

---
+++ 09.EF-Core-Essentials-CinemaApp/CinemaApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using CinemaApp.Contracts;
+using CinemaApp.Data;
+using CinemaApp.Data.Common;
+using CinemaApp.Models;
+using CinemaApp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true)
+    .AddUserSecrets(typeof(Program).Assembly)
+    .Build();
+
+// inversion of control container == serviceProvider
+var serviceProvider = new ServiceCollection()
+    .AddLogging()
+    .AddDbContext<CinemaDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("CinemaConnection")))
+    .AddScoped<IRepository, Repository>() // ако в някакъв момент искаме да сменим Entity Framework, можем да променим само <IRepository, Repository> 
+    .AddScoped<ICinemaService, CinemaService>()
+    .BuildServiceProvider();
+
+using var scope = serviceProvider.CreateScope();
+ICinemaService? service = scope.ServiceProvider.GetService<ICinemaService>();
+
+if (service != null)
+{
+    var cinema = new CinemaModel("Capitol", "Sofia, Car Boris III");
+
+    await service.AddCinemaAsync(cinema);
+}
