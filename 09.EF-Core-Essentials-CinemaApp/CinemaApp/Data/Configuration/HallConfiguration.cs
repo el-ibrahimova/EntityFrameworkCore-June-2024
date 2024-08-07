0b0b7b251b4b18eee5b2e6b87ee305166150227c
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-07 19:04:20 +0300

    create configuration for Cinema and Hall

---
+++ 09.EF-Core-Essentials-CinemaApp/CinemaApp/Data/Configuration/HallConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using CinemaApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CinemaApp.Data.Configuration
+{
+    public class HallConfiguration : IEntityTypeConfiguration<Hall>
+    {
+        public void Configure(EntityTypeBuilder<Hall> builder)
+        {
+            builder
+                .HasData(
+                    new Hall()
+                    {
+                        Id = 1,
+                        Name = "IMAX Hall 1",
+                        CinemaId = 1
+                    },
+                    new Hall()
+                    {
+                        Id = 2,
+                        Name = "IMAX Hall 2",
+                        CinemaId = 1
+                    },
+                    new Hall()
+                    {
+                        Id = 3,
+                        Name = "VIP Hall",
+                        CinemaId = 2
+                    },
+                    new Hall()
+                    {
+                        Id = 4,
+                        Name = "3D Hall",
+                        CinemaId = 2
+                    },
+                    new Hall()
+                    {
+                        Id = 5,
+                        Name = "IMAX Hall 5",
+                        CinemaId = 3
+                    }
+                    );
+        }
+    }
+}
