1c00bde3210d834115219a7188be12e976fcba71
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-07 19:05:14 +0300

    create ModelBuilderExtension

---
+++ 09.EF-Core-Essentials-CinemaApp/CinemaApp/Data/Extension/ModelBuilderExtension.cs
@@ -0,0 +1,16 @@
+﻿using CinemaApp.Data.Configuration;
+using CinemaApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Data.Extention
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CinemaConfiguration());
+
+            modelBuilder.ApplyConfiguration(new HallConfiguration());
+        }
+    }
+}
