7d25e7535026ceb236e7431993f2acdbdb77788f
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-10 22:36:30 +0300

    create dto

---
+++ 10.BestPracticesAndArchitecture-CinemaApp/CinemaApp.Core/Models/DTOs/CinemaHallsExportDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace CinemaApp.Core.Models.DTOs
+{
+    public class CinemaHallsExportDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int NumberOfHalls { get; set; }
+    }
+}
