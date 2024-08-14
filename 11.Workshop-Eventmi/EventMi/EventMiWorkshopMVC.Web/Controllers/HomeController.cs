3a4117da5ab1b55a66e6a36d82b452fc3fbc1efb
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-14 20:24:15 +0300

    create project

---
+++ 11.Workshop-Eventmi/EventMi/EventMiWorkshopMVC.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using EventMiWorkshopMVC.Web.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EventMiWorkshopMVC.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController()
+        {
+           
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
