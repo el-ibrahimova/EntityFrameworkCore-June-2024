4897b95973f0b7501b60c178e4de486d9cbb49c8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-20 22:54:49 +0300

    Create IEventService.cs

---
+++ 11.Workshop-Eventmi/EventMi/EventMiWorkshopMVC.Services.Data/Interfaces/IEventService.cs
@@ -0,0 +1,15 @@
+﻿using EventMiWorkshopMVC.Web.ViewModels.Event;
+
+namespace EventMiWorkshopMVC.Services.Data.Interfaces
+{
+    public interface IEventService
+    {
+        Task AddEvent(AddEventFormModel eventFormModel, DateTime startDate, DateTime endDate);
+
+        Task<EditEventFormModel> GetEventById(int id);
+
+        Task EditEventById(int id, EditEventFormModel eventFormModel, DateTime startDate, DateTime endDate);
+
+        Task DeleteEventById(int id);
+    }
+}
