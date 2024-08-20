97a9ba9e6b3bb2839737b6b8d7afe6df11781cff
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-08-20 22:54:46 +0300

    Create EditEventFormModel.cs

---
+++ 11.Workshop-Eventmi/EventMi/EventMiWorkshopMVC.Web.ViewModels/Event/EditEventFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventMiWorkshopMVC.Web.ViewModels.Event
+{
+    using static Common.EntityConstraints;
+
+    public class EditEventFormModel
+    {
+        
+        [Required]
+        [StringLength(EventNameMaxLength, MinimumLength = EventNameMinLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string StartDate { get; set; } = null!;
+
+        [Required]
+        public string EndDate { get; set; } = null!;
+
+        [Required]
+        [StringLength(EventPlaceMaxLength, MinimumLength = EventPlaceMinLength)]
+        public string Place { get; set; } = null!;
+    }
+}
